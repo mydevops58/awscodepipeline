1f88fe538e9b3fe91469cd1258bb60c5857c5087
mydevops58 <naveenrthumma58@gmail.com>
2025-08-04 03:16:26 -0500

    codechange

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello from Node.js app running on ECS Fargate!');
+  res.send('Hello from Node.js app running on ECS Fargate! deployed through AWS CodePipeline');
 });
 
 app.listen(port, () => {
